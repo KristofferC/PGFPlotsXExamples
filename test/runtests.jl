8fae5391018761561ec3338def04e606e894b0ca
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-05-13 11:39:12 +0200

    update_testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,41 @@
 using PGFPlotsXExamples
+import PGFPlotsX
+
 using Base.Test
 using NBInclude
+using LaTeXStrings
+using Contour
+using Colors
+using DataFrames
 
 const EXAMPLE_DIR = joinpath(@__DIR__, "..", "examples/")
 
-for file in readdir(EXAMPLE_DIR)
-    if endswith(file, ".ipynb")
-        println("Testing ", file)
-        nbinclude(joinpath(EXAMPLE_DIR, file))
+const MIME_types = [PGFPlotsX.Axis, PGFPlotsX.TikzPicture, PGFPlotsX.Plot, PGFPlotsX.TikzDocument]
+
+function export_ans(x)
+    if typeof(x) in MIME_types
+        folder, file = tempdir(), tempname()
+        pdffile = joinpath(folder, file) * ".pdf"
+        println("saving ", typeof(x), " to ", joinpath(folder, file), ".pdf")
+        PGFPlotsX.save(pdffile, x)
+        rm(pdffile)
+    else
+        println("skipping $(typeof(x))")
+    end
+end
+
+
+cd(EXAMPLE_DIR) do
+    for file in readdir()
+        if endswith(file, ".ipynb")
+            println("Testing ", file)
+            if get(ENV, "CI", false)
+                println("Setting f to ...")
+                f = identity
+            else
+                f = export_ans
+            end
+            nbinclude(joinpath(EXAMPLE_DIR, file); anshook = f)
+        end
     end
 end
